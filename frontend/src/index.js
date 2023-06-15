040eb32e5b9c535f5e48a28d1c2b0aa1aaaf5799
anthonytkool <anthonytcool@hotmail.com>
2023-06-15 21:23:38 +0700

    redux store setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom'
-
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import App from './App'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import reportWebVitals from './reportWebVitals'
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,13 +22,15 @@ const router = createBrowserRouter(
       <Route path='/product/:id' element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
-)
+);
 
-reportWebVitals()
+reportWebVitals();
